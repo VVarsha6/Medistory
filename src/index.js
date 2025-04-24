3aecb8da0b2c946019c1284802d9838c34eefdc1
Leo dos Remedios <leondosr@outlook.com>
2025-04-23 21:05:33 -0500

    removed stuff :3 - Leo

--- src/index.js
+++ src/index.js
@@ -2,21 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Landing from './landing';
-import NoteGen from './noteGen';
-
-const Routing = () => {
-    return(
-      <Router>
-          <Routes>
-              <Route path="/" element={<Landing />}/>
-              <Route path="/App" element = {<App />}/>
-              <Route path="/noteGen" element = {<NoteGen />}/>
-          </Routes>
-      </Router>
-    )
-}
-export default Routing
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Routing />);
+root.render(<App />);
